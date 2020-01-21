ec865b8286134b663fa6a2f2f92aaeb370106eac
bhargav.geddam <g.bhargav007@yahoo.com>
2020-01-21 23:43:00 +0530

    Day1

---
+++ djangoProjects/bhargavDjango/calc/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import  HttpResponse
+# Create your views here.
+
+def home(request):
+    #return HttpResponse("helloWorld")
+    return render(request,'home.html',{'name' : 'BHargav'})
+
+def add(request):
+    val1  = request.GET['num1']
+    val2  = request.GET['num2']
+    res = int(val1)+int(val2)
+
+
+    return render(request,"result.html",{'result':res})
